9e33b201ec89b814f20406eb4910a4e137954c46
ParkByeongJun <degea98@gmail.com>
2024-10-26 18:27:34 +0900

    react 프로젝트와 연결하기 위해 cors 활성화

--- src/main.ts
+++ src/main.ts
@@ -1,21 +1,29 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as dotenv from 'dotenv';
-import {Logger} from "nestjs-pino";
-import {ValidationPipe} from "@nestjs/common";
+import { Logger } from 'nestjs-pino';
+import { ValidationPipe } from '@nestjs/common';
 
 dotenv.config();
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  //app.useLogger(app.get(Logger))
+
+  // CORS 활성화
+  app.enableCors({
+    origin: 'http://localhost:3001', // React 앱의 URL
+    credentials: true,
+  });
+
   app.useGlobalPipes(new ValidationPipe({
-    whitelist:true,
+    whitelist: true,
     forbidNonWhitelisted: true,
-    transformOptions:{
-      enableImplicitConversion: true
-    }
-  }))
+    transformOptions: {
+      enableImplicitConversion: true,
+    },
+  }));
+
   await app.listen(3000);
 }
+
 bootstrap();
